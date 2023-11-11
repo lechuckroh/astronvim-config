e4fbbf77aab211c4251d1a9fdeb1c04f8099e10c
Lechuck Roh <lechuckroh@gmail.com>
2023-11-11 22:22:25 +0900

    customize plugins

--- plugins/user.lua
+++ plugins/user.lua
@@ -1,12 +1,18 @@
 return {
-  -- You can also add new plugins here as well:
-  -- Add plugins, the lazy syntax
-  -- "andweeb/presence.nvim",
-  -- {
-  --   "ray-x/lsp_signature.nvim",
-  --   event = "BufRead",
-  --   config = function()
-  --     require("lsp_signature").setup()
-  --   end,
-  -- },
+  {
+    "folke/todo-comments.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    opts = {},
+    event = "User AstroFile",
+    cmd = { "TodoQuickFix" },
+    keys = {
+      { "<leader>T", "<cmd>TodoQuickFix<cr>", desc = "Open TODOs" },
+    },
+  },
+  {
+    "folke/trouble.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    opts = {},
+    event = "User AstroFile",
+  },
 }
